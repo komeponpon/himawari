58c95523efb6acd8fdc9e983d3f4045f390779ff
komeponpon <ricemountain1030@gmail.com>
2024-10-08 15:55:52 +0900

    localでの作業には対応

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the backend with TypeScript!');
+  res.send('Hello, World!');
 });
 
 app.listen(port, () => {
