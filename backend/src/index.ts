af30c225902b6516d9cc76be3db75341beaf29c7
komeponpon <ricemountain1030@gmail.com>
2024-10-09 15:00:30 +0900

    solar-systemsエンドポイントの設定

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+// Prisma Clientを使用したデータベース操作の例
+app.get('/solar-systems', async (req, res) => {
+  const solarSystems = await prisma.solar_system.findMany();
+  res.json(solarSystems);
 });
 
 app.listen(port, () => {
