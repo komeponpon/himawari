ccfdc5b7c80c80ecdeef6132ae4e05266abd0f02
komeponpon <ricemountain1030@gmail.com>
2024-11-25 15:37:14 +0900

    CORS修正

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -13,7 +13,8 @@ app.use((0, cors_1.default)({
         'http://localhost:3000',
         'http://192.168.0.191:3000',
         'http://frontend:3000',
-        'http://localhost:5001'
+        'http://localhost:5001',
+        'http://192.168.0.191:5001'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://192.168.0.191:3000',
     'http://frontend:3000',
-    'http://localhost:5001'
+    'http://localhost:5001',
+    'http://192.168.0.191:5001'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
