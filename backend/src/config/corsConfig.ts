9f8f76a83fe4f0af5ffb9bf7d5527b6b752a1aaf
komeponpon <ricemountain1030@gmail.com>
2024-12-04 15:57:22 +0900

    cors

--- backend/src/config/corsConfig.ts
+++ backend/src/config/corsConfig.ts
@@ -2,11 +2,8 @@ import cors from 'cors';
 
 export const corsOptions = {
   origin: [
-    'http://localhost:3000',
-    'http://192.168.0.191:3000',
-    'http://frontend:3000',
-    'http://localhost:5001',
-    'http://192.168.0.191:5001'
+    'http://34.97.52.199:3000',
+    'http://34.97.52.199:5001',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
